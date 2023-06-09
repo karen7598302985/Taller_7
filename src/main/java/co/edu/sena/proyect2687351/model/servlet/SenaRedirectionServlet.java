6dadef7df4fbf58a075507bc29c6b373e39eeef1
KarenPortillo2687351 <124842168+karen7598302985@users.noreply.github.com>
2023-06-08 21:23:38 -0500

    Add files via upload

---
+++ src/main/java/co/edu/sena/proyect2687351/model/servlet/SenaRedirectionServlet.java
@@ -0,0 +1,20 @@
+package co.edu.sena.proyect2687351.model.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/Ssena")
+public class SenaRedirectionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("https://www.sena.edu.co");
+    }
+
+}
